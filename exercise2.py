b87ea7425609d1244deb5eb80ccdcf1f19c2bbc9
TRACY <tracysallaway@gmail.com>
2014-09-29 20:29:03 -0400

    Exercise2 testing passed - 09/29

--- exercise2.py
+++ exercise2.py
@@ -6,18 +6,15 @@
     Assignment 1, Exercise 2, INF1340 Fall 2014
 """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
+__author__ = 'Tracy Sallaway, Sarah-Anne Schultheis'
+__email__ = "tracy.armstrong@mail.utoronto.ca, sarah.schultheis@mail.utoronto.ca"
 
-__copyright__ = "2014 Susan Sim"
-__license__ = "MIT License"
-
-__status__ = "Prototype"
+__copyright__ = "2014 Tracy Sallaway and Sarah-Anne Schultheis"
 
 # imports one per line
 
 
-def checksum (upc):
+def checksum(upc):
     """
     Checks if the digits in a UPC is consistent with checksum
 
@@ -26,23 +23,46 @@ def checksum (upc):
         Boolean: True, checksum is correct
         False, otherwise
     :raises:
-        TypeError if input is not a strong
-        ValueError if string is the wrong length (with error string stating how many digits are over or under
+        TypeError if input is not a string
+        ValueError if string is the wrong length (with error string stating how many digits are over or under)
     """
 
-    # check type of input
-    # raise TypeError if not string
+    # check type of input and raise TypeError if not string
+    if len(upc) != 12:
+        raise ValueError('UPC values must be exactly 12 characters')
+    if type(upc) != str:
+        raise TypeError('Input must be of type string')
 
-    # check length of string
-    # raise ValueError if not 12
+    #input_type = type(upc)
+    #if input_type != str:
+        #print("TypeError: Input must be of type string")
+    #else:
+        #print("Type Correct")
+
+    # check length of string and raise ValueError if not 12
+    #input_length = len(upc)
+    #if input_length != 12:
+        #print("ValueError: Input must be 12 characters in length")
+    #else:
+        #print("Length Correct")
 
     # convert string to array
-    # hint: use the list function
+    list(upc)
 
-    # generate checksum using the first 11 digits provided
-    # check against the the twelfth digit
+    # generate checksum with first 11 digits provided
+    # Note: odd positions denoted as even and vice versa due to Python index position 0
+    odd_upc = ((int(upc[0]) + int(upc[2]) + int(upc[4]) + int(upc[6]) + int(upc[8]) + int(upc[10])) * 3)
+    even_upc = odd_upc + (int(upc[1]) + int(upc[3]) + int(upc[5]) + int(upc[7]) + int(upc[9]))
+    final_checksum = even_upc % 10
 
-    # return True if they are equal, False otherwise
+    # check against twelfth digit, return True if equal, False otherwise
+    if final_checksum == 0:
+        final_checksum = final_checksum
+    else:
+        final_checksum = 10 - (int(final_checksum))
 
-    return False
+    if int(final_checksum) == int(upc[11]):
+        return True
+    else:
+        return False
 
