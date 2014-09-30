aa31caaa79df2d764c978f39bfd5847c6cc5b99c
TRACY <tracysallaway@gmail.com>
2014-09-29 20:30:00 -0400

    Exercise2 testing passed - 09/29

--- test_exercise2.py
+++ test_exercise2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 """ Module to test exercise2.py """
+import pytest
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
+__author__ = 'Tracy Sallaway, Sarah-Anne Schultheis'
+__email__ = "tracy.armstrong@mail.utoronto.ca, sarahschultheis@mail.utoronto.ca"
 
 __copyright__ = "2014 Susan Sim"
 __license__ = "MIT License"
@@ -12,10 +13,7 @@ __status__ = "Prototype"
 
 # imports one per line
 
-# imports one per line
-import pytest
 from exercise2 import checksum
-
 
 def test_checksum():
     """
@@ -24,20 +22,24 @@ def test_checksum():
     assert checksum("786936224306") is True
     assert checksum("085392132225") is True
     assert checksum("717951000841") is False
-    # other tests
+    assert checksum("892685001003") is True
+    assert checksum("012345678905") is True
+    assert checksum("692771017440") is True
+    assert checksum("827624391275") is False
+    assert checksum("012345678901") is False
 
 
 def test_input():
-    """
-    Inputs that are the incorrect format and length
-    """
+   # """
+    #Inputs that are the incorrect format and length
+    #"""
     with pytest.raises(TypeError):
-        checksum(1.0)
-        checksum(786936224306)
+       checksum(1.0)
+       checksum(786936224306)
 
     with pytest.raises(ValueError):
-        checksum("1")
-        checksum("1234567890")
+       checksum("1")
+       checksum("1234567890")
 
     # other tests
 
